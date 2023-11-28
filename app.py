6ba152180053f7dfbd3773713735086e506207d1
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-11-28 15:01:21 -0800

    copy-pasted flask boilerplate

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from boggle import Boggle
 
+from flask import Flask, session, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app=Flask(__name__)
+app.config["SECRET_KEY"] = "shhhh!!"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+debug = DebugToolbarExtension(app)
+
 boggle_game = Boggle()
