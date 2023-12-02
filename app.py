b9907636b29377755a802fdfc8697528b622bc8c
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-12-01 16:49:35 -0800

    added route to display game board

--- app.py
+++ app.py
@@ -9,3 +9,9 @@ app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+@app.route("/")
+def display_index():
+    if not session.get("board"):
+        session["board"] = boggle_game.make_board()
+    return render_template("gameboard.html")
