2258b1147ba9792b978c66607839b932459a5a42
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-12-01 20:22:02 -0800

    doubled the appearance rate of vowels

--- boggle.py
+++ boggle.py
@@ -24,7 +24,10 @@ class Boggle():
         board = []
 
         for y in range(5):
-            row = [choice(string.ascii_uppercase) for i in range(5)]
+            # increase the commonality of vowels
+            alphabet = string.ascii_uppercase + "AEIOU"
+            
+            row = [choice(alphabet) for i in range(5)]
             board.append(row)
 
         return board
